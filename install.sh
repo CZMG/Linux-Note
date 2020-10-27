10612a3d12e01576f3f8ceab58b57a6ccc9d53d9
CZMG <1007653544@qq.com>
2020-10-27 09:46:01 +0800

    Add files via upload
    get dvwa
    get vimplus
    get oh-my-zsh
    get xp

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# @Author: WYF
+# @Date:   last_modified_time
+# @Last Modified by:   10076
+# @Last Modified time: last_modified_time
+
+apt update && apt upgrade -y
+apt install zsh
+
+# dvwa
+wget https://github.com/ethicalhack3r/DVWA/archive/master.zip
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# xp
+wget -O install.sh https://download.xp.cn/install.sh && sudo bash install.sh
+# vimplus
+git clone https://github.com/chxuan/vimplus.git ~/.vimplus
+cd ~/.vimplus
+./install.sh
